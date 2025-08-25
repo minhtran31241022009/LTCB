8d4b1b39456930a98e37e9151d6cc706a305b669
Tran Trieu Minh <minhtran.31241022009@st.ueh.edu.vn>
2025-08-26 01:10:10 +0700

    BT4_2808_31241022009

--- src/BT4_31241022009.py
+++ src/BT4_31241022009.py
@@ -9,10 +9,10 @@ win, lose = 0, 0
 print(f"You have {money}$")
 while True:
     print("Computer thinks a number from 1 to 100")
+    print("In each game, you should pay 5$")
     comp_num = random.randint(1, 100)
     level = int(input("Choose the level [1-easy],[2-medium],[3-hard]: "))
     times = 10 if level == 1 else 5 if level == 2 else 3
-    bet = int(input("Enter your bet: "))
     is_win = False
     for time in range(times):
         your_num = int(input("Enter your guessing number #"+str(time+1)+": "))
@@ -26,14 +26,14 @@ while True:
         if is_win and time +1 <= times:
             print("You win!")
             win += 1
-            money += bet
+            money += 5
             print("----------------------------------")
             print(f"Your money is {money} now")
             break
         if time +1 >= times:
             print("You lose!")
             lose += 1
-            money -= bet
+            money -= 5
             print("----------------------------------")
             print(f"Your money is {money} now")
     if money <= 0:
